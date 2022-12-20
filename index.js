9297a65f621e12198ffdea952dca47fedf71786c
Drako <ahmad2003madina@gmail.com>
2022-12-20 16:20:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).on("click", "btn", function(){
+    alert('Hello');
+});
+
+function Mycode() {
+    name = 'Ahmed'
+    if (name != null){
+    aler("Ahmed Is a starter Programmer!");
+    }
+    }
